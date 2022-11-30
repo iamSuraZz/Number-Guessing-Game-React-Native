b6916bdecbe6c48510ae4fc21124945a90ac12a3
SuraZz007 <surajdev9886@gmail.com>
2022-11-30 19:55:05 +0530

    create custom buttons and style numeric input.

---
+++ components/PrimaryButton.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+function PrimaryButton({ children }) {
+  return (
+    <View>
+      <Text>{children}</Text>
+    </View>
+  );
+}
+export default PrimaryButton;
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,43 @@
+import { TextInput, View, StyleSheet } from "react-native";
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        keyboardType="numeric"
+        maxLength={2}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    padding: 16,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    marginTop: 100,
+    backgroundColor: "#72063c",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
